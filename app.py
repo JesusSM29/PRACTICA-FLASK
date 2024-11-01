4e7b65250e6ada6dd1d0a06a0d2fca25cb7da819
JesusSM29 <jesusguillermosm29@gmail.com>
2024-10-31 22:18:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,146 +1,47 @@
-from flask import Flask
-
-from flask import render_template
-from flask import request
-from flask import jsonify, make_response
-
-import pusher
-
-import mysql.connector
-import datetime
-import pytz
-
-con = mysql.connector.connect(
-    host="185.232.14.52",
-    database="u760464709_tst_sep",
-    user="u760464709_tst_sep_usr",
-    password="dJ0CIAFF="
-)
+from flask import Flask, request, render_template, flash, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'tu_clave_secreta_aqui'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'pdf'}
 
-@app.route("/")
-def index():
-    con.close()
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-    return render_template("app.html")
+@app.route('/', methods=['GET'])
+def mostrar_formulario():
+    return render_template('pago_curso.html')
 
-@app.route("/alumnos")
-def alumnos():
-    con.close()
-
-    return render_template("alumnos.html")
-
-@app.route("/alumnos/guardar", methods=["POST"])
-def alumnosGuardar():
-    con.close()
-    matricula      = request.form["txtMatriculaFA"]
-    nombreapellido = request.form["txtNombreApellidoFA"]
-
-    return f"Matrícula {matricula} Nombre y Apellido {nombreapellido}"
-
-# Código usado en las prácticas
-def notificarActualizacionTemperaturaHumedad():
-    pusher_client = pusher.Pusher(
-        app_id="1714541",
-        key="2df86616075904231311",
-        secret="2f91d936fd43d8e85a1a",
-        cluster="us2",
-        ssl=True
-    )
-
-    pusher_client.trigger("canalRegistrosTemperaturaHumedad", "registroTemperaturaHumedad", args)
-
-@app.route("/buscar")
-def buscar():
-    if not con.is_connected():
-        con.reconnect()
-
-    cursor = con.cursor(dictionary=True)
-    cursor.execute("""
-    SELECT Id_Log, Temperatura, Humedad, DATE_FORMAT(Fecha_Hora, '%d/%m/%Y') AS Fecha, DATE_FORMAT(Fecha_Hora, '%H:%i:%s') AS Hora FROM sensor_log
-    ORDER BY Id_Log DESC
-    LIMIT 10 OFFSET 0
-    """)
-    registros = cursor.fetchall()
-
-    con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/guardar", methods=["POST"])
-def guardar():
-    if not con.is_connected():
-        con.reconnect()
-
-    id          = request.form["id"]
-    temperatura = request.form["temperatura"]
-    humedad     = request.form["humedad"]
-    fechahora   = datetime.datetime.now(pytz.timezone("America/Matamoros"))
+@app.route('/procesar_pago', methods=['POST'])
+def procesar_pago():
+    if 'comprobante' not in request.files:
+        flash('No se envió ningún archivo')
+        return redirect(url_for('mostrar_formulario'))
     
-    cursor = con.cursor()
+    file = request.files['comprobante']
+    if file.filename == '':
+        flash('No se seleccionó ningún archivo')
+        return redirect(url_for('mostrar_formulario'))
 
-    if id:
-        sql = """
-        UPDATE sensor_log SET
-        Temperatura = %s,
-        Humedad     = %s
-        WHERE Id_Log = %s
-        """
-        val = (temperatura, humedad, id)
+    telefono = request.form.get('telefono')
+    confirmar_telefono = request.form.get('confirmarTelefono')
+
+    if telefono != confirmar_telefono:
+        flash('Los números de teléfono no coinciden')
+        return redirect(url_for('mostrar_formulario'))
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Pago registrado correctamente')
+        return redirect(url_for('mostrar_formulario'))
     else:
-        sql = """
-        INSERT INTO sensor_log (Temperatura, Humedad, Fecha_Hora)
-                        VALUES (%s,          %s,      %s)
-        """
-        val =                  (temperatura, humedad, fechahora)
-    
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
+        flash('Tipo de archivo no permitido')
+        return redirect(url_for('mostrar_formulario'))
 
-    notificarActualizacionTemperaturaHumedad()
-
-    return make_response(jsonify({}))
-
-@app.route("/editar", methods=["GET"])
-def editar():
-    if not con.is_connected():
-        con.reconnect()
-
-    id = request.args["id"]
-
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    SELECT Id_Log, Temperatura, Humedad FROM sensor_log
-    WHERE Id_Log = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
-    registros = cursor.fetchall()
-    con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/eliminar", methods=["POST"])
-def eliminar():
-    if not con.is_connected():
-        con.reconnect()
-
-    id = request.form["id"]
-
-    cursor = con.cursor(dictionary=True)
-    sql    = """
-    DELETE FROM sensor_log
-    WHERE Id_Log = %s
-    """
-    val    = (id,)
-
-    cursor.execute(sql, val)
-    con.commit()
-    con.close()
-
-    notificarActualizacionTemperaturaHumedad()
-
-    return make_response(jsonify({}))
+if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    app.run(debug=True)
